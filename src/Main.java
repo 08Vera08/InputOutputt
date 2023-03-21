0563388a3bcb8df291976fb2c161d5fde7a5b882
Vera Sorokina <vera.sorokina.2013@mail.ru>
2023-03-21 11:22:55 +0400

    Two

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.io.File;
 import java.util.ArrayList;
 
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
         int N = 6;
         ArrayList<String> products = new ArrayList<String>();
@@ -11,13 +12,15 @@ public class Main {
         products.add("Яйцо");
         products.add("Рыба");
         products.add("Яблоко");
-        ArrayList<Integer> prices = new ArrayList<>();
+        products.add("Тушенка");
+        ArrayList<Integer> prices = new ArrayList<Integer>();
         prices.add(20);
         prices.add(150);
         prices.add(100);
         prices.add(80);
         prices.add(200);
         prices.add(100);
+        prices.add(10000);
         ArrayList<Integer> counts = new ArrayList<Integer>();
         counts.add(3);
         counts.add(4);
@@ -25,32 +28,37 @@ public class Main {
         counts.add(10);
         counts.add(2);
         counts.add(6);
+        counts.add(100);
         boolean mask[] = {true, true, true};
-        File textFile = new File("Basket.txt");
+        //File textFile = new File("Basket.txt");
+        File textFile = new File("Basket.bin");
         Basket basket;
-        if (textFile.exists()) {
-            basket = new Basket();
-            basket = basket.loadFromTxtFile(textFile);
-            System.out.println(basket.getSize());
-            for (int i = 0; i < N; ++i) {
+        if(textFile.exists()) {
+            System.out.println("Exist!");
+            //basket = basket.loadFromTxtFile(textFile);
+            basket = Basket.loadFromBinFile(textFile);
+            for(int i = 0; i < N; ++i){
                 boolean f = true;
-                for (int j = 0; j < basket.getSize(); ++j) {
-                    if (products.get(i).equals(basket.getProduct(j))) {
+                for(int j = 0; j < basket.getSize(); ++j){
+                    if(products.get(i).equals(basket.getProduct(j))){
                         basket.addToCart(j, counts.get(i));
                         f = false;
                         break;
                     }
                 }
-                if (f) {
+                if(f){
                     basket.setProduct(basket.getSize(), products.get(i));
                     basket.setPrice(basket.getSize(), prices.get(i));
                     basket.setCount(basket.getSize(), counts.get(i));
                 }
             }
-            basket.saveTxt(textFile);
-        } else {
+            //basket.saveTxt(textFile);
+            basket.saveBin(textFile);
+        }
+        else{
+            System.out.println("Not exist!");
             basket = new Basket(prices, counts, products, N);
-            basket.saveTxt(textFile);
+            basket.saveBin(textFile);
         }
         basket.printCart();
     }
